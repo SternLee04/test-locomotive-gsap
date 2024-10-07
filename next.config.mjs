729d099fcedad82a3dfd597b468bf97c77599af2
Stern_Lee_04 <jayvadalia04@gmail.com>
2024-10-07 15:44:56 +0530

    production version-1

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,28 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @format
+ * @type {import('next').NextConfig}
+ */
+
+import {setupDevPlatform} from "@cloudflare/next-on-pages/next-dev"
 const nextConfig = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.(mp4|webm|ogg|avi|mov)$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          publicPath: '/_next/static/media/',
-          outputPath: 'static/media/',
-          name: '[name].[hash].[ext]',
-        },
-      },
-    });
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.(mp4|webm|ogg|avi|mov)$/,
+			use: {
+				loader: "file-loader",
+				options: {
+					publicPath: "/_next/static/media/",
+					outputPath: "static/media/",
+					name: "[name].[hash].[ext]",
+				},
+			},
+		})
 
-    return config;
-  },
-};
+		return config
+	},
+}
+if (process.env.NODE_ENV === "development") {
+	await setupDevPlatform()
+}
 
-export default nextConfig;
+export default nextConfig
